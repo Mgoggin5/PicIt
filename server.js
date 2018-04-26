99350cf5a627fe3c26d4597088475a051a327b8e
Anant Patel <Anant1234@hotmail.com>
2018-04-26 14:19:08 -0400

    now connecting to localhost

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 var db = require("./models");
+
+
+
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
