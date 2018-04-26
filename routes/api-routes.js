afb63ec4fbea086f4e8026f1830f24a11fd6027c
Matthew Goggin <matthewgoggin@Matthews-MacBook-Pro.local>
2018-04-26 14:59:04 -0400

    html

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,50 +4,50 @@
 
 var db = require("../models")
 
-module.exports = function(app){
+module.exports = function (app) {
 
 
-  app.get("/api/posts", function(req, res) {
-      db.Post.findAll({}).then(function(dbPost) {
-      res.json(dbPost);
+  app.get("/api/travelinfos", function (req, res) {
+    db.travelinfo.findAll({}).then(function (dbtravelinfo) {
+      res.json(dbtravelinfo);
     });
   });
 
-  app.get("/api/posts/:id", function(req, res){
-      db.Post.findOne({
-          where: {id: req.params.id}
-      }).then(function(dbPost){
-          res.json(dbPost)
-      });
-  });
- 
-app.post("/api/posts", function(req, res){
-    db.Post.create(req.body).then(function(dbPost){
-        res.json(dbPost)
+  app.get("/api/travelinfos/:id", function (req, res) {
+    db.travelinfo.findOne({
+      where: { id: req.params.id }
+    }).then(function (dbtravelinfo) {
+      res.json(dbtravelinfo)
     });
-})
+  });
+
+  app.travelinfo("/api/travelinfos", function (req, res) {
+    db.travelinfo.create(req.body).then(function (dbtravelinf) {
+      res.json(dbtravelinfo)
+    });
+  })
 
 
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post.destroy({
+  app.delete("/api/travelinfos/:id", function (req, res) {
+    db.travelinfo.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbPost) {
-      res.json(dbPost);
+    }).then(function (dbtravelinf) {
+      res.json(dbtravelinfo);
     });
   });
 
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post.update(
+  // PUT route for updating travelinfos
+  app.put("/api/travelinfos", function (req, res) {
+    db.travelinfo.update(
       req.body,
       {
         where: {
           id: req.body.id
         }
-      }).then(function(dbPost) {
-      res.json(dbPost);
-    });
+      }).then(function (dbtravelinfo) {
+        res.json(dbtravelinfo);
+      });
   });
 }
