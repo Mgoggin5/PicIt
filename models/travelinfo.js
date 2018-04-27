0964d77d00f5c2f1cf16ee564c36f43d2a38e5d7
Anant Patel <Anant1234@hotmail.com>
2018-04-27 13:49:49 -0400

    adding info to datatbase working

--- models/travelinfo.js
+++ models/travelinfo.js
@@ -1,22 +1,25 @@
 module.exports = function(sequelize, DataTypes) {
-    var travelinfo = sequelize.define("travelinfo", {
-      email: {
-        type: DataTypes.STRING,
-      },
-      destination: {
-        type: DataTypes.TEXT,
-      },
-      attractions: {
-        type: DataTypes.TEXT,
-      },
-      activities: {
-        type: DataTypes.TEXT,
-      },
-      destination: {
-        type: DataTypes.TEXT,
-      }
-    });  
+    var Travelinfo = sequelize.define("travelinfo", {
+        email: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        destination: {
+          type: DataTypes.TEXT,
+        },
+        attractions: {
+          type: DataTypes.TEXT,
+        },
+        activities: {
+          type: DataTypes.TEXT,
+        },
+        restaurants: {
+          type: DataTypes.TEXT,
+        },
+        }, {
+          timestamps: false
+        });  
   
-    return travelinfo;
+    return Travelinfo;
   };
   
