56066ae0cc4b58209475346a19584032b574fc53
Wayne Wright <wwiinavi@hotmail.com>
2018-04-27 11:12:49 -0400

    update

--- models/travelinfo.js
+++ models/travelinfo.js
@@ -2,9 +2,12 @@ module.exports = function(sequelize, DataTypes) {
     var travelinfo = sequelize.define("travelinfo", {
       email: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: { len: [6] }
       },
       destination: {
         type: DataTypes.TEXT,
+        allowNull: false,
       },
       attractions: {
         type: DataTypes.TEXT,
@@ -12,11 +15,10 @@ module.exports = function(sequelize, DataTypes) {
       activities: {
         type: DataTypes.TEXT,
       },
-      destination: {
-        type: DataTypes.TEXT,
-      }
+      // destination: {
+      //   type: DataTypes.TEXT,
+      // }
     });  
   
     return travelinfo;
-  };
-  
+  }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,14 +2,14 @@ var path = require("path") // to connect to local directories
 
 module.exports = function(app){
     app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    app.get("/locale", function(req, res){
-        res.sendFile(path.join(__dirname, '../public/locale.html'));
+    app.get("/landing", function(req, res){
+        res.sendFile(path.join(__dirname, '../public/landing.html'));
     });
 
-    app.get("/post", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/post.html"));
+    app.get("/view_dest", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/view_dest.html"));
     });
 }
