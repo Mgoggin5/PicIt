7fd951e72e8d12a6099637f372d80ac98a1c475b
Anant Patel <Anant1234@hotmail.com>
2018-04-29 18:50:13 -0400

    updated addinfo.js

--- public/js/addinfo.js
+++ public/js/addinfo.js
@@ -1,6 +1,6 @@
-// from unit 15 activity 5 library
 
-$("#submit-btn").on("click", function(event) {
+//working code for last record.
+  $("#submit-btn").on("click", function(event) {
     event.preventDefault();
   
     // Make a destination object
@@ -12,12 +12,9 @@ $("#submit-btn").on("click", function(event) {
       restaurants: $("#restInput").val().trim()      
     };
   
-    $.post("/api/new", newDestination)
-    // On success, run the following code
+    $.post("/api/new", newDestination)    
     .then(function(results) {
-    // Log the data we found
-    // console.log("results: ", results); //working
-    
+        
     // from unit 15 activity 1 chirp.js
     $.ajax({
       method: "GET",
@@ -28,29 +25,33 @@ $("#submit-btn").on("click", function(event) {
       showtravelinfo(data);  
     });
 
-    function showtravelinfo(dbtravelinfo){
-      $("#entry").empty();
-      for (let i = 0; i < dbtravelinfo.length; i++) {
-        const element = dbtravelinfo[i];  
-        console.log("showtraveelinfo() element: ", element)
-        addOneEntry(element);
-      }
-    }
+    function showtravelinfo(dbtravelinfo){      
+          $("#entry").empty();
+          var argv;
+          for (let i = 0; i < dbtravelinfo.length; i++) {
+            arvg =  dbtravelinfo[dbtravelinfo.length - 1];
+          }
+          console.log("showtraveelinfo() dbtravelinfo.length: ", arvg);              
+          // return argv;
+          addOneEntry(arvg);          
+        }//showtravelinfo() ends here.        
 
-    function addOneEntry(userEntry){
+    function addOneEntry(test){
+      console.log("addOneEntry () argv: ", test);
       var newEntryElement = $("<div>");
       newEntryElement.addClass("entryclass");
-      newEntryElement.append(`<p>id: ${userEntry.id}</p>`);
-      newEntryElement.append(`<p>Email: ${userEntry.email}</p>`);
-      newEntryElement.append(`<p>Destination: ${userEntry.destination}</p>`);
-      newEntryElement.append(`<p>Attractions: ${userEntry.attractions}</p>`);
-      newEntryElement.append(`<p>Activities: ${userEntry.activities}</p>`);
-      newEntryElement.append(`<p>Restaurants: ${userEntry.restaurants}</p>`);
+      // newEntryElement.append(`<p>id: ${userEntry.id}</p>`);
+      newEntryElement.append(`<p>Email: ${test.email}</p>`);
+      newEntryElement.append(`<p>Destination: ${test.destination}</p>`);
+      newEntryElement.append(`<p>Attractions: ${test.attractions}</p>`);
+      newEntryElement.append(`<p>Activities: ${test.activities}</p>`);
+      newEntryElement.append(`<p>Restaurants: ${test.restaurants}</p>`);
       $("#entry").append(newEntryElement);
       console.log("add one entry: ",newEntryElement)
-    }
+    }  
 
-    }); //$.post ends here.     
+    }); //$.post ends here.   
+
 
     // Empty each input box by replacing the value with an empty string
     $("#emailadd").val("");
@@ -62,5 +63,82 @@ $("#submit-btn").on("click", function(event) {
 
   });//submit button ends here.
 
+  $("#deleteEntry").on("click", function(event) {
+    event.preventDefault();
+
+  });
+
+  $("#deleteEntry").on("click", function(event) {
+    event.preventDefault();
+
+  });
+
+
+  // orig working code. Displays all data from the table.
+  // $("#submit-btn").on("click", function(event) {
+  //   event.preventDefault();
   
+  //   // Make a destination object
+  //   var newDestination = {
+  //     email: $("#emailadd").val().trim(),
+  //     destination: $("#destInput").val().trim(),
+  //     attractions: $("#attrInput").val().trim(),
+  //     activities: $("#actiInput").val().trim(),
+  //     restaurants: $("#restInput").val().trim()      
+  //   };
+  
+  //   $.post("/api/new", newDestination)
+  //   // On success, run the following code
+  //   .then(function(results) {
+  //   // Log the data we found
+  //   // console.log("results: ", results); //working
+    
+  //   // from unit 15 activity 1 chirp.js
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "/api/entry"
+  //   }).then(function(data){
+  //     console.log("*********************");
+  //     console.log("ajax request: ", data);
+  //     showtravelinfo(data);  
+  //   });
+
+  //   function showtravelinfo(dbtravelinfo){
+  //     $("#entry").empty();
+  //     for (let i = 0; i < dbtravelinfo.length; i++) {
+  //       const element = dbtravelinfo[i];  
+  //       console.log("showtraveelinfo() element: ", element)
+  //       addOneEntry(element);
+  //     }
+  //   }
+
+  //   function addOneEntry(userEntry){
+  //     var newEntryElement = $("<div>");
+  //     newEntryElement.addClass("entryclass");
+  //     newEntryElement.append(`<p>id: ${userEntry.id}</p>`);
+  //     newEntryElement.append(`<p>Email: ${userEntry.email}</p>`);
+  //     newEntryElement.append(`<p>Destination: ${userEntry.destination}</p>`);
+  //     newEntryElement.append(`<p>Attractions: ${userEntry.attractions}</p>`);
+  //     newEntryElement.append(`<p>Activities: ${userEntry.activities}</p>`);
+  //     newEntryElement.append(`<p>Restaurants: ${userEntry.restaurants}</p>`);
+  //     $("#entry").append(newEntryElement);
+  //     console.log("add one entry: ",newEntryElement)
+  //   }
+
+  //   }); //$.post ends here.     
+
+  //   // Empty each input box by replacing the value with an empty string
+  //   $("#emailadd").val("");
+  //   $("#destInput").val("");
+  //   $("#attrInput").val("");
+  //   $("#actiInput").val("");
+  //   $("#restInput").val("");    
+
+
+  // });//submit button ends here.
+
+
+
+
+
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,77 +2,49 @@ var db = require("../models")
 
 module.exports = function(app){
 
-
-  // app.get("/api/travelinfo", function(req, res) {
-  //     db.travelinfo.findAll({}).then(function(dbtravelinfo) {
-  //     res.json(dbtravelinfo);
-  //   });
-  // });
-
-  // app.get("/api/travelinfo/:id", function(req, res){
-  //     db.travelinfo.findOne({
-  //         where: {id: req.params.id}
-  //     }).then(function(dbtravelinfo){
-  //         res.json(dbtravelinfo)
-  //     });
-  // });
- 
-// app.travelinfo("/api/travelinfo", function(req, res){
-//     db.travelinfo.create(req.body).then(function(dbtravelinfo){
-//         res.json(dbtravelinfo)
-//     });
-// })
-
-
-  // app.delete("/api/travelinfo/:id", function(req, res) {
-  //   db.travelinfo.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(dbtravelinfo) {
-  //     res.json(dbtravelinfo);
-  //   });
-  // });
-
-  // PUT route for updating travelinfos
-  // app.put("/api/travelinfo", function(req, res) {
-  //   db.travelinfo.update(
-  //     req.body,
-  //     {
-  //       where: {
-  //         id: req.body.id
-  //       }
-  //     }).then(function(dbtravelinfo) {
-  //     res.json(dbtravelinfo);
-  //   });
-  // });
-
   // adding new travel info to the database.
   app.post("/api/new", function(req, res) {
-    db.travelinfo.create(req.body).then(function(dbtravelinfo) {
-      res.json(dbtravelinfo);
-    });
-    // console.log("app.post req.body: ", req.body); // working
-  });   
+    db.travelinfo.create(req.body).then(function(result) {
+      // res.end();
+      res.json(result);
+    });    
+  });
 
-  // working   
+  // gets all user entries.
   app.get("/api/entry", function(req, res) {
     db.travelinfo.findAll(req.body).then(function(dbtravelinfo) {
       res.json(dbtravelinfo);
   });
-  //console.log (req.body) returns{}; no data.
-  console.log("app.get api/entry: ",req.body);
+   console.log("app.get api/entry: ",req.body);
 });
-
-  //  app.get("/api/entry", function(req, res) {
-  //     db.travelinfo.findAll(req.body).then(function(err, result) {
-  //       if (erro) throw err;
-  //       res.json(dbtravelinfo);
-  //   });
-  //   //console.log (req.body) returns{}; no data.
-  //   console.log("app.get api/entry: ", data);
-  // });
   
-    
+  // delte for deleting record.
+  app.delete("/api/delete/:id", function(req, res) {
+    db.travelinfo.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
 
-}
+  //PUT route for updating travelinfos
+  app.put("/api/update", function(req, res) {
+    db.travelinfo.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbtravelinfo) {
+      res.json(dbtravelinfo);
+    });
+  });
+
+  
+  
+  
+
+
+}// module export function app ends here.
