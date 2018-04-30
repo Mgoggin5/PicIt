2a46a3acc358834a2c54bc9f0d46153899533ce0
Matthew Goggin <matthewgoggin@Matthews-MacBook-Pro.local>
2018-04-30 14:45:02 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -2,28 +2,6 @@
 $(document).ready(function() {
     var emailInput = $("#email");
 
-    $(document).on("submit", "#author-form", handleAuthorFormSubmit);// from author.js
-
-    // Getting the initial list of Authors
-    getAuthors();
-
-    // A function to handle what happens when the form is submitted to create a new Author
-  function handleAuthorFormSubmit(event) {
-    event.preventDefault();
-    // Don't do anything if the name fields hasn't been filled out
-    if (!nameInput.val().trim().trim()) {
-      return;
-    }
-    // Calling the upsertAuthor function and passing in the value of the name input
-    upsertAuthor({
-      name: nameInput
-        .val()
-        .trim()
-    });
-  }
-
-});
-
 
 // INSERT LOTTO FUNCTION CODE 
 $(".btn btn-default").on("click", "#random-button", function() {
@@ -45,4 +23,5 @@ $(".btn btn-default").on("click", "#random-button", function() {
   else{
 
   }
+});
 });
