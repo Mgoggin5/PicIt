ee3b15585a999d2505cfd2d350d5047b1e3a4c68
Wayne Wright <wwiinavi@hotmail.com>
2018-05-03 19:22:31 -0400

    addinfo.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-$(function(){
-    $("#lotto").on("click", function(){
-        var email = $("#email").val().trim();
-        localStorage.setItem("email, email")
-    })
-});
+// $(function(){
+//     $("#lotto").on("click", function(){
+//         var email = $("#email").val().trim();
+//         localStorage.setItem("email, email")
+//     })
+// });
 
 // INSERT LOTTO FUNCTION CODE 
 $("#lotto").on("click", function() {
   var userLottoNum = "";
   var systemLottoNum = "6";
   var email = $("#email").val().trim();
-
+  localStorage.setItem("email", email)
   for (var i = 0; i < 9; i++) {
       var random = Math.floor(Math.random() * 10) + 1;
       userLottoNum = random + userLottoNum;
