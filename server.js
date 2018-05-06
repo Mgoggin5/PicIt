8be65d2e788063545cf7e8ee5abfdb99bf30f621
Matthew Goggin <matthewgoggin@Matthews-MBP.fios-router.home>
2018-05-06 17:01:22 -0400

    mysql

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser")
+var mysql = require("mysql")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
